dca41d4274a26df99bdbbc4d1c764a7dd6e62116
Dreik3 <nik.dolmatov@gmail.com>
2018-02-08 18:53:17 +0300

    final version

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
-/*var count = 0;
-
-$('#cat').click(function(){
-    console.log(count);
-    count += 1;
-    $('#count').replaceWith('<span id="count">' + count + '</span>');
-});
-*/
-
+var allCatList = $(".list");
+var catFromList = $("li");
+var areaForCats = $('.area');
 //constructor for cats
 function Cat(name, image) {
     this.name = name;
@@ -23,12 +17,14 @@ catsArray[3] = new Cat("Liza", "images/cat4.jpeg");
 catsArray[4] = new Cat("Ksenia", "images/cat5.jpg");
 console.log(catsArray);
 // add all our cats in array
+
 catsArray.forEach(function(cat, i, catsArray){
-    $(".list").append("<li id='" + i + "'><a href='#'>" + catsArray[i].name + "</a></li>");
+    allCatList.append("<li id='" + i + "'><a href='#'>" + catsArray[i].name + "</a></li>");
 });
 // array for cats which we clicked
+
 var chosenArray = [];
-$("li").click(function(){
+catFromList.click(function(){
     var catId = $(this).attr('id');
     var check = false;
     for (i = 0; i < chosenArray.length; i++){
@@ -43,7 +39,7 @@ $("li").click(function(){
     // display cats from chosenArray (only if not displayed yet)
     chosenArray.forEach(function(cat, i, chosenArray){
         if (chosenArray[i].onScreen === false){
-            $('.area').append("<div class='cats' id='cat" + i + "'></div>");
+            areaForCats.append("<div class='cats' id='cat" + i + "'></div>");
             $('#cat' + i).append("<h2>" + chosenArray[i].name + "</h2>");
             $('#cat' + i).append("<img height='400' width='500' id='catImg" + i + "' src='" + chosenArray[i].image + "'>");
             $('#cat' + i).append("<p>You clicked on this cat <span id='count" + i + "'>0</span> times!</p><br>");
