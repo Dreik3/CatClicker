51aa89c79025cf905005429ce43c80b4ac9223bf
Dreik3 <nik.dolmatov@gmail.com>
2018-02-14 18:05:50 +0300

    version with view-octupus-model structure

--- js/app.js
+++ js/app.js
@@ -1,55 +1,66 @@
+
 var allCatList = $(".list");
-var areaForCats = $('.area');
+var areaForCats = $(".area");
 //constructor for cats
-function Cat(name, image) {
+
+function Cat(id,name, image) {
+    this.id = id;
     this.name = name;
     this.image = image;
     this.count = 0;
-    this.onScreen = false;
-}
-var catsArray = [];
-//initializing cats
-catsArray.push(new Cat("Eddy", "images/cat1.jpg"));
-catsArray.push(new Cat("Diana", "images/cat2.jpg"));
-catsArray.push(new Cat("Baron", "images/cat3.jpeg"));
-catsArray.push(new Cat("Liza", "images/cat4.jpeg"));
-catsArray.push(new Cat("Ksenia", "images/cat5.jpg"));
-console.log(catsArray);
-// add all our cats in array
+    //this.onScreen = false;
+};
+var model = {
+    currentCat: null,
+    catsArray: [
+            new Cat(1,"Eddy", "images/cat1.jpg"),
+            new Cat(2,"Diana", "images/cat2.jpg"),
+            new Cat(3,"Baron", "images/cat3.jpeg"),
+            new Cat(4,"Liza", "images/cat4.jpeg"),
+            new Cat(5,"Ksenia", "images/cat5.jpg")
+                ]
+};
 
-catsArray.forEach(function(cat, i, catsArray){
-    allCatList.append(`<li id="${i}"><a href='#'>${catsArray[i].name}</a></li>`);
-});
-// array for cats which we clicked
-var catFromList = $("li");
-
-var chosenArray = [];
-catFromList.click(function(){
-    var catId = $(this).attr('id');
-    var check = false;
-    for (i = 0; i < chosenArray.length; i++){
-        if (chosenArray[i] === catsArray[catId]){
-            check = true;
-        };
-    };
-    if (check === false) {
-        chosenArray.push(catsArray[catId]);
+var octopus = {
+    init : () => {
+        model.catsArray.forEach(function(cat){
+            view.createList(cat);
+        });
+    },
+    setCurrentCatById: (id) => {
+        model.currentCat = model.catsArray.find((cat) => cat.id == id);
+        view.renderCat(model.currentCat);
     }
-    console.log(chosenArray);
-    // display cats from chosenArray (only if not displayed yet)
-    chosenArray.forEach(function(cat, i, chosenArray){
-        if (chosenArray[i].onScreen === false){
-            areaForCats.append("<div class='cats' id='cat" + i + "'></div>");
-            $('#cat' + i).append("<h2>" + chosenArray[i].name + "</h2>");
-            $('#cat' + i).append("<img height='400' width='500' id='catImg" + i + "' src='" + chosenArray[i].image + "'>");
-            $('#cat' + i).append("<p>You clicked on this cat <span id='count" + i + "'>0</span> times!</p><br>");
-            $('#catImg' + i).click(function(){
-                chosenArray[i].count += 1;
-                $('#count' + i).replaceWith('<span id="count'+ i +'">' + chosenArray[i].count + '</span>');
-            });
-            chosenArray[i].onScreen = true;
-        };
+};
 
-    });
-});
+var view = {
+    createList: (cat) => {
+        allCatList.append(`<li><a href='#'  id="${cat.id}" >${cat.name}</a>  </li>`);
+    },
+    choseCat: () => {
+        allCatList.click(function(){
 
+        })
+    },
+    renderCat: (cat) => {
+        areaForCats.html('');
+        areaForCats.append(`<div class='cats' id='cat${cat.id}'></div>`);
+        debugger;
+        $(`#cat${cat.id}`).append(`<h2> ${cat.name} </h2>`);
+        $(`#cat${cat.id}`).append(`<img height='400' width='500' id='catImg${cat.id}' src='${cat.image}'>`);
+        $(`#cat${cat.id}`).append(`<p>You clicked on this cat <span id='count${cat.id}'>${cat.count}</span> times!</p><br>`);
+
+        $(`#catImg${cat.id}`).click(function(){
+            cat.count++;
+            $(`#count${cat.id}`).replaceWith(`<span id="count${cat.id}">${cat.count} </span>`);
+        });
+
+    },
+    initEvents: () => {
+        allCatList.click((e) => {
+            octopus.setCurrentCatById(e.target.id);
+        })
+    }
+};
+octopus.init();
+view.initEvents();
